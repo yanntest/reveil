3a551e00ee56b099c79754436ce70554ee0223fd
Fred78560 <csi-entreprise@hotmail.fr>
2017-12-02 18:54:02 +0100

    Add files via upload

--- musique/aleatoire.bat
+++ musique/aleatoire.bat
@@ -1,19 +1,7 @@
 @echo off
- 
-set /a max=0
- 
-set /a nbr=0
- 
-rem Compte le nombre de fichier dans le répertoire
- 
-For %%a In ("*.*") Do set /a max+=1
- 
-rem Choisit un nombre aléatoire entre 1 et le nombre de fichier
- 
+set /a max=19
+set /a nbr=19
+For %%a In ("*.*") Do set /a max+1
 set /a nbr=%random%%%%max%
- 
-rem Met en variable le nom du fichier qui correspond au nombre aléatoire
-for /F "tokens=2 delims=]" %%i in ('dir /A:-D /B ^| find /N "." ^| find "%nbr%"') do set fichier=%%i
- 
-"%fichier%"
+start C:\SARAH\plugins\reveil\musique\%nbr%.mp3
 exit
